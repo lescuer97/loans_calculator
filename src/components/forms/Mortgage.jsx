7ceb5012113048b41872ac1f89a7e012d363ece4
Leonardo Escuer <leoescuer@protonmail.com>
2020-10-29 17:49:30 +0100

    wasm and js calculator
    Signed-off-by: Leonardo Escuer <leoescuer@protonmail.com>

--- src/components/forms/Mortgage.jsx
+++ src/components/forms/Mortgage.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
+
 import calculator from "../../util/calculator";
+import Table from "../pages/Table";
 
 const Mortgage = () => {
+  const [wasm, setWasm] = useState(null);
+
+  // loads wasm to the app for calculation
+  const loadedWasm = async () => {
+    try {
+      const pack = await import("workers");
+      setWasm(pack);
+    } catch (err) {
+      console.log("couldn't import wasm");
+    }
+  };
+
+  loadedWasm();
+
   const [hip, setHip] = useState({
     precio: "",
     prestamo: "",
@@ -9,7 +25,8 @@ const Mortgage = () => {
     interes: "",
     email: "",
   });
-  const [done, setDone] = useState(false);
+
+  const [morti, setMorti] = useState([]);
 
   const { precio, prestamo, años, interes, email } = hip;
 
@@ -25,108 +42,117 @@ const Mortgage = () => {
     const data = {
       precio: hip.precio * 1,
       prestamo: hip.prestamo * 1,
-      años: hip.años * 1,
+      anos: hip.años * 1,
       interes: hip.interes * 1,
     };
+    const date1 = Date.now();
+    const info = wasm.mort_calculator(data);
+    // const info = calculator(data);
+    console.log(info);
+    const compare = Date.now() - date1;
+    console.log(compare);
 
-    const total = calculator(data);
-    console.log(total);
+    // const total = calculator(data);
+
+    // setMorti(total);
   };
   return (
-    <div className='max-w-sm'>
-      <form className='p-6 flex flex-col justify-center' onSubmit={onSubmit}>
-        <h1 className='text-gray-800 font-semibold '> Calcula tu Hipoteca</h1>
-        <div className='flex flex-col'>
-          {/* <label htmlFor='name' className='hidden'>
+    <>
+      <div className='max-w-sm border border-solid '>
+        <form className='p-6 flex flex-col justify-center' onSubmit={onSubmit}>
+          <h1 className='text-gray-800 font-semibold '> Calcula tu Hipoteca</h1>
+          <div className='flex flex-col'>
+            {/* <label htmlFor='name' className='hidden'>
               Full Name
             </label> */}
-          <input
-            type='number'
-            name='precio'
-            id='precio'
-            value={precio}
-            onChange={onChange}
-            placeholder='precio de casa'
-            className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
-          />
-        </div>
-        <div className='flex flex-col mt-2'>
-          {/* <label htmlFor='tel' className='hidden'>
+            <input
+              type='number'
+              name='precio'
+              id='precio'
+              value={precio}
+              onChange={onChange}
+              placeholder='precio de casa'
+              className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
+          </div>
+          <div className='flex flex-col mt-2'>
+            {/* <label htmlFor='tel' className='hidden'>
               Number
             </label> */}
-          <input
-            type='number'
-            name='prestamo'
-            id='prestamo'
-            value={prestamo}
-            placeholder='cuanto pediras prestado?'
-            onChange={onChange}
-            className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
-          />
-        </div>
-        <div className='flex flex-col'>
-          {/* <label htmlFor='name' className='hidden'>
+            <input
+              type='number'
+              name='prestamo'
+              id='prestamo'
+              value={prestamo}
+              placeholder='cuanto pediras prestado?'
+              onChange={onChange}
+              className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
+          </div>
+          <div className='flex flex-col'>
+            {/* <label htmlFor='name' className='hidden'>
               Full Name
             </label> */}
-          <input
-            type='number'
-            name='años'
-            id='años'
-            value={años}
-            onChange={onChange}
-            placeholder='Años de Hipoteca'
-            className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
-          />
-        </div>
-        <div className='flex flex-col'>
-          {/* <label htmlFor='name' className='hidden'>
+            <input
+              type='number'
+              name='años'
+              id='años'
+              value={años}
+              onChange={onChange}
+              placeholder='Años de Hipoteca'
+              className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
+          </div>
+          <div className='flex flex-col'>
+            {/* <label htmlFor='name' className='hidden'>
               Full Name
             </label> */}
-          <input
-            type='number'
-            name='interes'
-            id='interes'
-            value={interes}
-            onChange={onChange}
-            placeholder='tasa de interes'
-            className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
-          />
-        </div>
+            <input
+              type='number'
+              name='interes'
+              id='interes'
+              value={interes}
+              onChange={onChange}
+              placeholder='tasa de interes'
+              className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
+          </div>
 
-        <div className='flex flex-col mt-2'>
-          {/* <label htmlFor='email' className='hidden'>
+          <div className='flex flex-col mt-2'>
+            {/* <label htmlFor='email' className='hidden'>
               Email
             </label> */}
-          <input
-            type='email'
-            name='email'
-            id='email'
-            placeholder='Email'
-            value={email}
-            onChange={onChange}
-            className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
-          />
-        </div>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              placeholder='Email'
+              value={email}
+              onChange={onChange}
+              className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
+          </div>
 
-        <button
-          type='submit'
-          className='md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
-          Submit
-        </button>
+          <button
+            type='submit'
+            className='md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
+            Submit
+          </button>
+          {/* 
+          <button
+            type='button'
+            onClick={(e) => {
+              e.preventDefault();
 
-        <button
-          type='button'
-          onClick={(e) => {
-            e.preventDefault();
-
-            // let hi = wasm.add(2, 5);
-            // console.log("hello Workd", hi);
-          }}
-          className='md:w-64 bg-indigo-600 hover:bg-blue-dark text-white  text-center text-centerfont-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
-          WebAssembly
-        </button>
-      </form>
-    </div>
+              // console.log("hello Workd", hi);
+            }}
+            className='md:w-64 bg-indigo-600 hover:bg-blue-dark text-white  text-center text-centerfont-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
+            WebAssembly
+          </button> */}
+        </form>
+      </div>
+      {morti ? <Table mort={morti} /> : <div></div>}
+    </>
   );
 };
 
--- src/util/calculator.js
+++ src/util/calculator.js
@@ -1,48 +1,64 @@
 const calculator = (form) => {
-  let { precio, interes, años, prestamo } = form;
+  let { precio, interes, anos, prestamo } = form;
+
   //interes en base 0
   const inter = interes / 100;
 
   const base = prestamo;
+
   //cuanto paga de interes
   const pagoint = base * (inter / 12);
+  console.log("pago int: ", pagoint);
 
   // amortizacion anual
-  const amortpago = Math.pow(1 + inter / 12, 12 * años);
+  const amortpago = Math.pow(1 + inter / 12, 12 * anos);
+  console.log("amortpago: ", amortpago);
 
   // calcula el pago mensual
   const pagoMensual = (pagoint * amortpago) / (amortpago - 1);
+  console.log("pagoMensual :", pagoMensual);
 
   let total = [];
 
+  // pago total de amortización
+  let totalPagoAmor = 0;
+
   //conto de meses de transasction
   let mes = 0;
+  let pagoTotInt = 0;
+  let id = 0;
 
   for (let i = prestamo; i >= 0; i) {
     if (i.toFixed(0) <= 0 && prestamo.toFixed(0) * 1 <= 0) {
       break;
     }
-    //cuanto paga de interes
+    //cuanto paga de interes y el total
     const pagoint = prestamo * (inter / 12);
+
+    pagoTotInt = pagoTotInt + pagoint;
 
     const menosBlc = pagoMensual - pagoint;
 
+    totalPagoAmor = totalPagoAmor + menosBlc;
+
     prestamo = prestamo - menosBlc;
-    console.log("prestamo :", prestamo.toFixed(2));
-    console.log("pago  interes :", pagoint.toFixed(2));
-    console.log("pago  amor :", menosBlc.toFixed(2));
+
     mes++;
 
-    let totmes = {
+    let objMes = {
       balance: prestamo.toFixed(2),
-      mes: mes.toFixed(2),
+      mes: mes.toFixed(0),
       pago_mesual: pagoMensual.toFixed(2),
       pago_interes: pagoint.toFixed(2),
       pago_amor: menosBlc.toFixed(2),
+      intereses_totales: pagoTotInt.toFixed(2),
+      pago_total_amor: totalPagoAmor.toFixed(2),
+      gasto_total: totalPagoAmor + pagoTotInt,
+      id: id++,
     };
 
     i = i - menosBlc;
-    total.push(totmes);
+    total.push(objMes);
   }
 
   return total;
