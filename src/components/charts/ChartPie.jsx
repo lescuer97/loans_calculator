3556595760c05d6693fad7f52f57e0120ad9a584
Leonardo Escuer <leoescuer@protonmail.com>
2020-11-06 10:11:43 +0100

    setting up final table data
    Signed-off-by: Leonardo Escuer <leoescuer@protonmail.com>

--- src/components/charts/ChartPie.jsx
+++ src/components/charts/ChartPie.jsx
@@ -5,10 +5,23 @@ import { PieChart, Pie, Cell, Legend } from "recharts";
 const ChartPie = (props) => {
   const data = useMemo(() => props.graphdat, [props.graphdat]);
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  let pago = data[0].value + data[1].value;
 
+  let { balance, pago_mensual } = props.mortiData;
+  // const { precio } = props.precio;
   return (
-    <>
-      <PieChart width={300} height={300}>
+    <div className='flex flex-col'>
+      <ul className=' flex flex-col'>
+        <li>
+          <strong>Tu cuota mensual:</strong> <span>{pago_mensual} €</span>
+        </li>
+        <li>
+          {" "}
+          <strong>Precio:</strong> <span>{props.precio} €</span>
+        </li>
+      </ul>
+
+      <PieChart width={400} height={300}>
         <Pie dataKey='value' data={data} label>
           {data.map((entry, index) => {
             return <Cell key={`cell-${index}`} fill={COLORS[index]} />;
@@ -18,7 +31,11 @@ const ChartPie = (props) => {
         <Legend verticalAlign='bottom' height={28} />
         {/* <Tooltip />; */}
       </PieChart>
-    </>
+      <p>
+        Pago Total:{" "}
+        {pago.toLocaleString("es-ES", { style: "currency", currency: "EUR" })}
+      </p>
+    </div>
   );
 };
 
--- src/components/forms/Mortgage.jsx
+++ src/components/forms/Mortgage.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useMemo } from "react";
-
+import _ from "lodash";
 import calculator from "../../util/calculator";
 import Table from "../pages/Table";
 import ChartPie from "../charts/ChartPie";
 
 const Mortgage = () => {
   const [wasm, setWasm] = useState(null);
-  //TODO agregar checkbox de opciones de hipoteca para mejores opciones
+  // for entire data table
+  const [mortiTable, setMortiTable] = useState(null);
+
+  // para datos legible
+  const [mortiData, setMortiData] = useState(null);
+
+  // para grafico
+  const [graphdat, setGraphdat] = useState(null);
 
   // loads wasm to the app for calculation
   const loadedWasm = async () => {
@@ -21,17 +28,14 @@ const Mortgage = () => {
   loadedWasm();
 
   const [hip, setHip] = useState({
-    precio: "",
-    prestamo: "",
-    años: "",
-    interes: "",
+    precio: 100000,
+    años: 30,
+    ahorro: 20000,
+    interes: 3,
     email: "",
   });
 
-  const [morti, setMorti] = useState(null);
-  const [graphdat, setGraphdat] = useState(null);
-
-  const { precio, prestamo, años, interes, email } = hip;
+  const { precio, ahorro, años, interes, email } = hip;
 
   const onChange = (e) => {
     setHip({
@@ -42,31 +46,32 @@ const Mortgage = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    //TODO agregar que el ahorro no puede ser mas que el precio
+
+    const prestamoCalc = hip.precio - hip.ahorro;
     const data = {
       precio: hip.precio * 1,
-      prestamo: hip.prestamo * 1,
+      prestamo: prestamoCalc,
       anos: hip.años * 1,
       interes: hip.interes * 1,
     };
-    const date = Date.now();
 
     //normal data
     // const normalData = calculator(data);
     const normalData = wasm.mort_calculator(data);
-    setMorti(normalData);
+
     const normalDataPop = normalData.pop();
-    console.log(normalDataPop);
 
     // calculates data with less price same year and interest
-    const data2 = data;
+    const data2 = _.clone(data);
     data2.prestamo = data2.prestamo * 0.8;
     // const lessPrice = calculator(data2);
     const lessPrice = wasm.mort_calculator(data2);
     const lessPricePop = lessPrice.pop();
-    console.log(lessPricePop);
 
     // calculates data with less years
-    const data3 = data;
+
+    const data3 = _.clone(data);
     data3.anos = data3.anos * 0.8;
     // const lessAnos = calculator(data3);
     const lessAnos = wasm.mort_calculator(data3);
@@ -74,30 +79,46 @@ const Mortgage = () => {
     console.log(lessAnosPop);
 
     // calculates data with less of everything
-    const data4 = data;
+    const data4 = _.clone(data);
     data4.anos = data4.anos * 0.8;
-    data4.interes = data4.interes * 0.8;
     data4.prestamo = data4.prestamo * 0.8;
-
     // const lessEvery = calculator(data4);
     const lessEvery = wasm.mort_calculator(data4);
     const lessEveryPop = lessEvery.pop();
-    console.log(lessEveryPop);
 
-    console.log(Date.now() - date);
-
-    // setGraphdat([
-    //   {
-    //     name: "Amor Total",
-    //     value: normalDataPop.pago_total_amor * 1,
-    //   },
-    //   {
-    //     name: "Interes Total",
-    //     value: normalDataPop.intereses_totales * 1,
-    //   },
-    // ]);
-    // console.log(normalDataPop);
-    // console.log(info);
+    // This is for the graphs
+    setGraphdat([
+      [
+        { name: "Amor Total", value: normalDataPop.pago_total_amor * 1 },
+        {
+          name: "Interes Total",
+          value: normalDataPop.intereses_totales * 1,
+        },
+      ],
+      [
+        { name: "Amor Total", value: lessPricePop.pago_total_amor * 1 },
+        {
+          name: "Interes Total",
+          value: lessPricePop.intereses_totales * 1,
+        },
+      ],
+      [
+        { name: "Amor Total", value: lessAnosPop.pago_total_amor * 1 },
+        {
+          name: "Interes Total",
+          value: lessAnosPop.intereses_totales * 1,
+        },
+      ],
+      [
+        { name: "Amor Total", value: lessEveryPop.pago_total_amor * 1 },
+        {
+          name: "Interes Total",
+          value: lessEveryPop.intereses_totales * 1,
+        },
+      ],
+    ]);
+    // This is for the final data
+    setMortiData([normalDataPop, lessPricePop, lessAnosPop, lessEveryPop]);
   };
   return (
     <>
@@ -105,9 +126,11 @@ const Mortgage = () => {
         <form className='p-6 flex flex-col justify-center' onSubmit={onSubmit}>
           <h1 className='text-gray-800 font-semibold '> Calcula tu Hipoteca</h1>
           <div className='flex flex-col'>
-            {/* <label htmlFor='name' className='hidden'>
-              Full Name
-            </label> */}
+            <label
+              className='block text-gray-700 text-sm font-bold '
+              htmlFor=''>
+              Precio del inmueble
+            </label>
             <input
               type='number'
               name='precio'
@@ -118,24 +141,44 @@ const Mortgage = () => {
               className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
             />
           </div>
+
           <div className='flex flex-col mt-2'>
-            {/* <label htmlFor='tel' className='hidden'>
-              Number
-            </label> */}
+            <label
+              className='block text-gray-700 text-sm font-bold '
+              htmlFor=''>
+              Ahorro Aportado
+            </label>
             <input
               type='number'
-              name='prestamo'
-              id='prestamo'
-              value={prestamo}
+              min={0}
+              max={precio}
+              name='ahorro'
+              id='ahorro'
+              value={ahorro}
               placeholder='cuanto pediras prestado?'
               onChange={onChange}
               className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
             />
+
+            <input
+              type='range'
+              min={0}
+              max={precio}
+              step={10000}
+              name='ahorro'
+              id='ahorro'
+              value={ahorro}
+              placeholder='cuanto pediras prestado?'
+              onChange={onChange}
+              className='w-100  py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
           </div>
           <div className='flex flex-col'>
-            {/* <label htmlFor='name' className='hidden'>
-              Full Name
-            </label> */}
+            <label
+              className='block text-gray-700 text-sm font-bold '
+              htmlFor=''>
+              Años de la Hipoteca
+            </label>
             <input
               type='number'
               name='años'
@@ -145,11 +188,24 @@ const Mortgage = () => {
               placeholder='Años de Hipoteca'
               className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
             />
+            <input
+              type='range'
+              min={0}
+              max={100}
+              step={1}
+              name='años'
+              id='años'
+              value={años}
+              onChange={onChange}
+              className='w-100  py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
           </div>
           <div className='flex flex-col'>
-            {/* <label htmlFor='name' className='hidden'>
-              Full Name
-            </label> */}
+            <label
+              className='block text-gray-700 text-sm font-bold '
+              htmlFor=''>
+              Tasa de Interes
+            </label>
             <input
               type='number'
               name='interes'
@@ -159,12 +215,25 @@ const Mortgage = () => {
               placeholder='tasa de interes'
               className='w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
             />
+            <input
+              type='range'
+              min={0}
+              max={30}
+              step={1}
+              name='interes'
+              id='interes'
+              value={interes}
+              onChange={onChange}
+              className='w-100  py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none'
+            />
           </div>
 
           <div className='flex flex-col mt-2'>
-            {/* <label htmlFor='email' className='hidden'>
+            <label
+              className='block text-gray-700 text-sm font-bold '
+              htmlFor=''>
               Email
-            </label> */}
+            </label>
             <input
               type='email'
               name='email'
@@ -181,25 +250,71 @@ const Mortgage = () => {
             className='md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
             Submit
           </button>
-          {/* 
-          <button
-            type='button'
-            onClick={(e) => {
-              e.preventDefault();
-
-              // console.log("hello Workd", hi);
-            }}
-            className='md:w-64 bg-indigo-600 hover:bg-blue-dark text-white  text-center text-centerfont-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300'>
-            WebAssembly
-          </button> */}
         </form>
       </div>
-      {/* <Table mort={morti} /> */}
-      <div className='mt-4'>
-        {/* <ChartPie /> */}
-        {!graphdat ? <div></div> : <ChartPie graphdat={graphdat} />}
+
+      <div className='mt-4 flex flex-row'>
+        <div>
+          {!graphdat ? (
+            <div></div>
+          ) : (
+            <>
+              <p className='h3 text-center '>Normal Data</p>
+              <ChartPie
+                graphdat={graphdat[0]}
+                mortiData={mortiData[0]}
+                precio={hip.precio}
+              />
+            </>
+          )}
+        </div>
+        <div>
+          {!graphdat ? (
+            <div></div>
+          ) : (
+            <>
+              <h3 className='h3 text-center '>Less loan amount</h3>{" "}
+              <ChartPie
+                graphdat={graphdat[1]}
+                mortiData={mortiData[1]}
+                precio={hip.precio}
+              />{" "}
+            </>
+          )}
+        </div>
+        <div className=''>
+          {!graphdat ? (
+            <div></div>
+          ) : (
+            <>
+              {" "}
+              <h3 className='h3 text-center '>Less years of mortgage</h3>{" "}
+              <ChartPie
+                graphdat={graphdat[2]}
+                mortiData={mortiData[2]}
+                precio={hip.precio}
+              />
+            </>
+          )}
+        </div>
+        <div className=''>
+          {!graphdat ? (
+            <div></div>
+          ) : (
+            <>
+              <h3 className='h3 text-center '>
+                Less loan amount and less years
+              </h3>{" "}
+              <ChartPie
+                graphdat={graphdat[3]}
+                mortiData={mortiData[3]}
+                precio={hip.precio}
+              />{" "}
+            </>
+          )}
+        </div>
       </div>
-      {/* {!morti ? <div></div> : <Table mort={morti} />} */}
+      {/* {!mortiTable ? <div></div> : <Table mort={mortiTable} />} */}
     </>
   );
 };
