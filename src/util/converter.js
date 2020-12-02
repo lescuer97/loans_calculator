81ef2e55ca2d9fb30a9a477fbf24eb354d919cee
Leonardo Escuer <leoescuer@protonmail.com>
2020-12-02 13:44:53 +0100

    adding ahorro aportado al coste final
    Signed-off-by: Leonardo Escuer <leoescuer@protonmail.com>

--- src/util/converter.js
+++ src/util/converter.js
@@ -1,4 +1,4 @@
-const  converter = (obj) => {
+const  converter = (obj, ahorro) => {
   obj.balance = ( obj.balance *1).toLocaleString("es-ES", { style: "currency", currency: "EUR" });
   // obj.balance =  obj.balance.toLocaleString("es-ES", { style: "currency", currency: "EUR" });
     obj.mes =  obj.mes * 1;
@@ -13,7 +13,7 @@ const  converter = (obj) => {
     // obj.intereses_totales =  obj.intereses_totales.toLocaleString("es-ES", { style: "currency", currency: "EUR" }); 
     obj.pago_total_amor =  (obj.pago_total_amor *1).toLocaleString("es-ES", { style: "currency", currency: "EUR" });
     // obj.pago_total_amor =  obj.pago_total_amor.toLocaleString("es-ES", { style: "currency", currency: "EUR" });
-    obj.gasto_total =   (obj.gasto_total *1).toLocaleString("es-ES", { style: "currency", currency: "EUR" }); 
+    obj.gasto_total =   ((obj.gasto_total *1) + (ahorro * 1)).toLocaleString("es-ES", { style: "currency", currency: "EUR" }); 
     // obj.gasto_total =   obj.gasto_total.toLocaleString("es-ES", { style: "currency", currency: "EUR" }); 
 return obj;
 }
