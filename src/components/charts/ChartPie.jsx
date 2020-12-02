7c45a71e3e62c97164743b294caef8f90723e2aa
Leonardo Escuer <leoescuer@protonmail.com>
2020-12-02 13:46:31 +0100

    removing comments
    Signed-off-by: Leonardo Escuer <leoescuer@protonmail.com>

--- src/components/charts/ChartPie.jsx
+++ src/components/charts/ChartPie.jsx
@@ -1,74 +1,35 @@
-import React, { useMemo, useEffect} from "react";
+import React, { useMemo, useEffect } from "react";
 // import { PieChart, Pie, Cell, Legend, Tooltip,  BarChart, Bar,  XAxis, YAxis, CartesianGrid } from "recharts";
 // TODO ADD ANIMATION
 const ChartPie = (props) => {
-  
-// useEffect(() => {})
+  // useEffect(() => {})
 
-  const data = useMemo(() =>  props.graphdat, [props.graphdat]);
+  const data = useMemo(() => props.graphdat, [props.graphdat]);
 
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
-
-
-  let { balance, pago_mensual, gasto_total, pago_total_amor, intereses_totales,  } = props.mortiData;
+  let {
+    balance,
+    pago_mensual,
+    gasto_total,
+    pago_total_amor,
+    intereses_totales,
+  } = props.mortiData;
 
   return (
-    <div className='flex flex-col py-1 '>
+    <div className="flex flex-col py-1 ">
       <div className="mb-2 px-5">
-        <p className="text-center" >Esta es tu cuota:</p>
-        <h1 className="text-center text-3xl text-green-500"><strong className="">{pago_mensual}</strong> / mes</h1>
+        <p className="text-center">Esta es tu cuota:</p>
+        <h1 className="text-center text-2xl lg:text-3xl text-green-500">
+          <strong className="">{pago_mensual}</strong> / mes
+        </h1>
       </div>
-      <div className="mb-2">
-        <p className="text-center" >Terminarias Pagando por la casa:</p>
-        <h1 className="text-center text-3xl"><strong className="text-red-500">{gasto_total}</strong></h1>
+      <div className="mb-1">
+        <p className="text-center">Terminarias Pagando por la casa:</p>
+        <h1 className="text-center text-2xl lg:text-3xl">
+          <strong className="text-red-500">{gasto_total}</strong>
+        </h1>
       </div>
-      {/* <ul className='list-inside flex flex-col'>
-        <div className="mb-2 ">
-        <div className="bg-green-300 ">
-        <li className="flex justify-between ml-2" >
-          <strong>Tu cuota mensual:</strong>
-           <p className="mr-3" >{pago_mensual}</p>
-        </li>
-             <li className="flex justify-between ml-2">
-          <strong>La casa vale:</strong> <span className="mr-3" >{props.precio}</span>
-        </li>
-        </div>
-        </div>
-        <div className="mt-2">
-        <div className="bg-red-500  ">
-        <li className="flex justify-between ml-2">
-            <strong>Pediste prestado:</strong> <span className="mr-3" >{pago_total_amor}</span>
-        </li>
-        <li className="flex justify-between ml-2">
-          {" "}
-          <strong>Terminaste Pagando: </strong> <span className="mr-3" >{gasto_total}</span>
-        </li>
-          <li className="flex justify-between ml-2">
-          {" "}
-          <strong> En Interes: </strong> <span className="mr-3" >{intereses_totales}</span>
-        </li>
-        </div>
-        </div>
-      </ul> */}
-      
-    <div className="mt-3">
-     {/* <PieChart width={400} height={350}>
-        <Pie dataKey='value' nameKey="name" data={data} legendType='circle' label labelLine>
-          {data.map((entry, index) => {
-            return <Cell key={`cell-${index}`} fill={COLORS[index]} />;
-          })}
-        </Pie>
-
-        <Legend verticalAlign='bottom' height={28} />
-        <Tooltip />;
-      </PieChart>   */}
- 
-      {/* <p>
-        Pago Total:{" "}
-        {pago.toLocaleString("es-ES", { style: "currency", currency: "EUR" })}
-      </p> */}
-    </div>
     </div>
   );
 };
